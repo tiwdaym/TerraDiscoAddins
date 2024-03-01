72274606771d6576bc96fb5f3fc7f633dd683261
tiwdaym <matej@ridzon.sk>
2024-03-02 00:12:43 +0100

    Update install_powershell.sh

--- setup/install_powershell.sh
+++ setup/install_powershell.sh
@@ -6,18 +6,23 @@
 sudo apt-get update
 
 # Install pre-requisite packages.
-sudo apt-get install -y wget
+sudo apt-get install -y wget apt-transport-https software-properties-common
 
-# Download the PowerShell package file
-wget https://github.com/PowerShell/PowerShell/releases/download/v7.4.1/powershell_7.4.1-1.deb_amd64.deb
+# Get the version of Ubuntu
+source /etc/os-release
+
+# Download the Microsoft repository keys
+wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+
+# Register the Microsoft repository keys
+sudo dpkg -i packages-microsoft-prod.deb
+
+# Delete the Microsoft repository keys file
+rm packages-microsoft-prod.deb
+
+# Update the list of packages after we added packages.microsoft.com
+sudo apt-get update
 
 ###################################
-# Install the PowerShell package
-sudo dpkg -i powershell_7.4.1-1.deb_amd64.deb
-
-# Resolve missing dependencies and finish the install (if necessary)
-sudo apt-get install -f
-
-# Delete the downloaded package file
-rm powershell_7.4.1-1.deb_amd64.deb
-
+# Install PowerShell
+sudo apt-get install -y powershell
